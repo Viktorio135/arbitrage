a7728cccea6811744f73560e1a8ecea6dc1f9329
Viktorio135 <102095652+Viktorio135@users.noreply.github.com>
2024-02-12 19:14:08 +0200

    test

---
+++ front/aiogram_main.py
@@ -0,0 +1,5 @@
+from aiogram import Bot, Dispatcher, types, filters
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.utils import executor
